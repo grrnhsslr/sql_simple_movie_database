b2dfd2fe12e9dba4d7b5199c24f1e2727c14b21a
grrnhsslr <hasslergarren@gmail.com>
2024-03-21 17:50:03 -0500

    made fixes

--- movietheater.sql
+++ movietheater.sql
@@ -1,28 +1,23 @@
--- create a table for movies
+-- create table for movies 
 CREATE TABLE IF NOT EXISTS movie(
-    movie_id serial PRIMARY KEY,
-    title varchar(50) NOT NULL,
-    description varchar(250)
+	movie_id serial PRIMARY KEY,
+	title varchar NOT NULL,
+	description varchar
 );
 
--- create a table for tickets
 CREATE TABLE IF NOT EXISTS tickets(
-    ticket_id serial PRIMARY KEY,
-    amount integer NOT NULL,
-    price integer,
-    movie_id integer,
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+	ticket_id serial PRIMARY KEY,
+	amount integer,
+	price integer
 );
 
--- create a table for customers
+ALTER TABLE tickets
+ADD FOREIGN KEY(movie_id) REFERENCES movie(movie_id);
+
+SELECT * FROM movie;
+
 CREATE TABLE IF NOT EXISTS customers(
-    customer_id serial PRIMARY KEY,
-    firstname varchar(20),
-    lastname varchar(20)
+	customer_id serial PRIMARY KEY,
+	firstname varchar(20),
+	lastname varchar(20)
 );
-
-ALTER TABLE customers 
-ADD COLUMN tickets_purchased integer NOT NULL;
-
-ALTER TABLE customers
-ADD FOREIGN KEY(tickets_purchased) REFERENCES movie(movie_id)
